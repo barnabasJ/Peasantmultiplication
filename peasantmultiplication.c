98f8fa86a58b5dbf439a3ef37fcb7d5a6eaee3ea
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2017-11-20 07:30:41 +0100

    ran code through clang-formatter

--- peasantmultiplication.c
+++ peasantmultiplication.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 
-int main()
-{
-	
-	int num1=0;
-	int num2=0;
-	int result=0;
+int main() {
 
-	printf("Enter first number!\n");
-	scanf("%d",&num1);
+  int num1 = 0;
+  int num2 = 0;
+  int result = 0;
 
-	printf("Enter second number!\n");
-	scanf("%d",&num2);
+  printf("Enter first number!\n");
+  scanf("%d", &num1);
 
-	if(num1%2!=0)		
-		result=result+num2;
+  printf("Enter second number!\n");
+  scanf("%d", &num2);
 
-	while(num1>1) {
-		num1=num1/2;
-		num2=num2*2;
-			if(num1%2!=0)
-				result=result+num2;
-}			
-	printf("The peasant multiplicated result is %d!",result);
+  if (num1 % 2 != 0)
+    result = result + num2;
 
+  while (num1 > 1) {
+    num1 = num1 / 2;
+    num2 = num2 * 2;
+    if (num1 % 2 != 0)
+      result = result + num2;
+  }
+  printf("The peasant multiplicated result is %d!", result);
 }
